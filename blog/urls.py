0f294aa653feba58fdf4cfc4da50a2e56b2bfdef
Diana Strahilova <diana.strahilova91.ds@gmail.com>
2024-11-06 20:31:41 +0000

    Deploy to heroku

--- blog/urls.py
+++ blog/urls.py
@@ -6,13 +6,13 @@ from django.views import generic
 
 urlpatterns = [
 
-     path('', Index.as_view(), name='home'),
-     path('add_recipe/', AddRecipe.as_view(), name='add_recipe'),
-     path('recipes/', RecipeView.as_view(), name='recipes'),
-     path('<slug:slug>/', views.recipe_detail, name='recipe'),
-     path('<slug:slug>/edit_comment/<int:comment_id>',
+    path('', Index.as_view(), name='home'),
+    path('add_recipe/', AddRecipe.as_view(), name='add_recipe'),
+    path('recipes/', RecipeView.as_view(), name='recipes'),
+    path('<slug:slug>/', views.recipe_detail, name='recipe'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
-     path('<slug:slug>/delete_comment/<int:comment_id>',
+    path('<slug:slug>/delete_comment/<int:comment_id>',
          views.comment_delete, name='comment_delete'),
      
     
